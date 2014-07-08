61cf4d5b6048d44b983c5019c68290060a5f8bba
Tyriar <tyriar@tyriar.com>
2014-07-08 06:51:38 -0700

    Add jshint rules, cleanup

--- abbr-fill.js
+++ abbr-fill.js
@@ -1,27 +1,27 @@
 /*! abbr-fill | (c) 2014 Daniel Imms | github.com/Tyriar/abbr-fill/blob/master/LICENSE */
- 
+
 abbrFill = (function () {
   var config;
 
-  return function (configuration) {
-    var i, j, key;
-
+  function init (configuration) {
     if (!configuration || !configuration.terms || !configuration.selector)
       return;
 
     config = configuration;
 
     var nodes = document.querySelectorAll(config.selector);
-    for (i = 0; i < nodes.length; i++) {
+    for (var i = 0; i < nodes.length; i++) {
       applyAbbrs(nodes[i]);
     }
   }
 
   function applyAbbrs(node) {
-    for (term in config.terms) {
-      // This will only match the first instance if there are two instances of the term separated
-      // by a space. This is an unlikely scenario and probably also not desired behaviour.
-      matchText(node, new RegExp("(^|\\s)" + term + "($|\\s|\.|,)", "g"), wrapElement);
+    for (var term in config.terms) {
+      // This will only match the first instance if there are two instances of
+      // the term separated by a space. This is an unlikely scenario and
+      // probably also not desired behaviour.
+      matchText(node, new RegExp("(^|\\s)" + term + "($|\\s|\\.|,)", "g"),
+          wrapElement);
     }
   }
 
@@ -39,14 +39,24 @@ abbrFill = (function () {
     node.parentNode.insertBefore(newElem, node.nextSibling);
   }
 
-  // Modified version of http://blog.alexanderdickson.com/javascript-replacing-text
-  function matchText(node, regex, callback, excludeElements) { 
+  // Modified version of
+  // http://blog.alexanderdickson.com/javascript-replacing-text
+  function matchText(node, regex, callback) {
     var excludeElements = ['script', 'style', 'iframe', 'canvas'];
     var child = node.firstChild;
 
-    // Required for <img> 
+    // Required for <img>
     if (!child)
       return node;
+
+    var replaceFunction = function(match) {
+      var args = [].slice.call(arguments),
+          offset = args[args.length - 2],
+          newTextNode = child.splitText(offset);
+      newTextNode.data = newTextNode.data.substr(match.length);
+      callback.apply(window, [child].concat(args));
+      child = newTextNode;
+    };
 
     do {
       switch (child.nodeType) {
@@ -56,19 +66,15 @@ abbrFill = (function () {
         }
         matchText(child, regex, callback, excludeElements);
         break;
-      case 3: // text node}
-        child.data.replace(regex, function(match) {
-          var args = [].slice.call(arguments),
-              offset = args[args.length - 2],
-              newTextNode = child.splitText(offset);
-          newTextNode.data = newTextNode.data.substr(match.length);
-          callback.apply(window, [child].concat(args));
-          child = newTextNode;
-        });
+      case 3: // text node
+        child.data.replace(regex, replaceFunction);
         break;
       }
-    } while (child = child.nextSibling);
+      child = child.nextSibling;
+    } while (child);
 
     return node;
   }
+
+  return init;
 })();
