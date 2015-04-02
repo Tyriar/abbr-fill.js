206104c8bc57c9b0e0fe4c251fced85e12d29429
Daniel Imms <tyriar@tyriar.com>
2015-04-01 19:14:22 -0700

    Add dist back

---
+++ dist/abbr-fill.js
@@ -0,0 +1,147 @@
+/*! find-and-wrap.js | (c) 2014 Daniel Imms | github.com/Tyriar/abbr-fill/blob/master/LICENSE */
+
+var findAndWrap = (function () {
+  'use strict';
+
+  function execute(container, term, wrappingTagName) {
+    wrappingTagName = wrappingTagName.toLowerCase();
+    return matchText(container,
+                     term,
+                     new RegExp('(^|\\s)' + term + '($|\\s|\\.|,)', 'g'),
+                     wrappingTagName);
+  }
+
+  function matchText(node, term, regex, wrappingTagName) {
+    var excludeElements = ['script', 'style', 'iframe', 'canvas'];
+    var child = node.firstChild;
+    var elements = [];
+
+    // Required for <img>, <hr>
+    if (!child) {
+      return elements;
+    }
+
+    var replaceFunction = function(match) {
+      var args = [].slice.call(arguments);
+      var offset = args[args.length - 2];
+      var newTextNode = child.splitText(offset);
+      newTextNode.data = newTextNode.data.substr(match.length);
+      var newElement = wrapElement.apply(
+          window, [child, term, wrappingTagName].concat(args));
+      if (newElement) {
+        elements = elements.concat(newElement);
+      }
+      child = newTextNode;
+    };
+
+    do {
+      switch (child.nodeType) {
+      case Node.ELEMENT_NODE:
+        if (excludeElements.indexOf(child.tagName.toLowerCase()) > -1) {
+          continue;
+        }
+        elements = elements.concat(
+            matchText(child, term, regex, wrappingTagName));
+        break;
+      case Node.TEXT_NODE:
+        child.data.replace(regex, replaceFunction);
+        break;
+      }
+      child = child.nextSibling;
+    } while (child);
+
+    return elements;
+  }
+
+  function wrapElement(node, term, wrappingTagName, match, offset) {
+    // TODO: Pull non-generic logic from here into abbr-fill.js
+    if (node.parentNode.tagName.toLowerCase() === wrappingTagName) {
+      // The element has either already been wrapped or existed without the
+      // script, just add the text back as it was removed by the regex
+      node.data = match;
+      return;
+    }
+
+    // First add the surrounding spaces matched with the regex back
+    if (match[0] === ' ') {
+      node.data += ' ';
+    }
+    if (match.length > 0 && match[match.length - 1] === ' ') {
+      node.nextSibling.data = ' ' + node.nextSibling.data;
+    }
+    // Create the new element
+    var matchText = match.trim();
+
+    var lastChar = matchText[matchText.length - 1];
+    if (lastChar === '.' || lastChar === ',') {
+      var trimmedEnd = matchText.substring(0, matchText.length - 1);
+      // only use if the term exists without the '.' or ',' at the end
+      if (trimmedEnd === term) {
+        matchText = trimmedEnd;
+        node.nextSibling.data = lastChar + node.nextSibling.data;
+      }
+    }
+
+    if (matchText !== term) {
+      throw 'Unexpected error: The text "' + matchText +
+          ' didn\'t match the term "' + term + '"';
+    }
+
+    var newElem = document.createElement(wrappingTagName);
+    newElem.innerHTML = matchText;
+    //newElem.title = config.terms[matchText];
+    node.parentNode.insertBefore(newElem, node.nextSibling);
+
+    return newElem;
+  }
+
+
+  return execute;
+})();
+
+/*! abbr-fill.js | (c) 2014 Daniel Imms | github.com/Tyriar/abbr-fill.js/blob/master/LICENSE */
+
+var abbrFill = (function (findAndWrap) {
+  'use strict';
+
+  var config;
+
+  function init (configuration) {
+    if (!configuration || !configuration.terms || !configuration.selector) {
+      return;
+    }
+
+    config = configuration;
+
+    var nodes = document.querySelectorAll(config.selector);
+    for (var i = 0; i < nodes.length; i++) {
+      applyAbbrs(nodes[i]);
+    }
+  }
+
+  function applyAbbrs(node) {
+    for (var term in config.terms) {
+      if (config.terms.hasOwnProperty(term)) {
+        // This will only match the first instance if there are two instances of
+        // the term separated by a space. Since it is quite an unlikely scenario
+        // and probably not desirable to wrap both anyway it's allowed.
+        // For example: "ABC ABC" -> "<abbr title="...">ABC</abbr> ABC
+        //matchText(node, new RegExp('(^|\\s)' + term + '($|\\s|\\.|,)', 'g'),
+        //    wrapElement);
+
+        var abbrs = findAndWrap(node, term, 'abbr');
+
+        if (!abbrs.length) {
+          continue;
+        }
+
+
+        for (var i = 0; i < abbrs.length; i++) {
+          abbrs[i].title = config.terms[term];
+        }
+      }
+    }
+  }
+
+  return init;
+})(findAndWrap);
